e96ea167e0e128a87edcad06d0c4540c1559db1d
Tatsuroh Sakaguchi <tacchan.mello.ioiq@gmail.com>
2018-11-02 14:57:44 +0900

    add photo entity

---
+++ src/main/java/memoworld/entities/Photo.java
@@ -0,0 +1,64 @@
+package memoworld.entities;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+@XmlRootElement
+public class Photo {
+    @XmlElement(name = "id")
+    private int id;
+    @XmlElement(name = "date")
+    private Date date;
+    @XmlElement(name = "description")
+    private String description;
+    @XmlElement(name = "location")
+    private String location;
+    @XmlElement(name = "author")
+    private String author;
+    @XmlElement(name = "raw_uri")
+    private String raw_uri;
+
+    public Photo() {
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRawURI() {
+        return raw_uri;
+    }
+
+    public void setRawURI(String raw_uri) {
+        this.raw_uri = raw_uri;
+    }
+}
