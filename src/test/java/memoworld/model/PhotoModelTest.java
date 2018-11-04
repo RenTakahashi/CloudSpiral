1505e6f9e5cfc3d586295c701d456528f6a87448
Tatsuroh Sakaguchi <tacchan.mello.ioiq@gmail.com>
2018-11-04 09:31:55 +0900

    add test for PhotoModel

---
+++ src/test/java/memoworld/model/PhotoModelTest.java
@@ -0,0 +1,51 @@
+package memoworld.model;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.sun.org.apache.bcel.internal.generic.POP2;
+import memoworld.entities.Location;
+import memoworld.entities.Photo;
+import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class PhotoModelTest {
+    private int itemCount;
+
+    @Before
+    public void setUp() throws Exception {
+        PhotoModel model = new PhotoModel();
+        itemCount = model.newId();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        MongoCollection<Document> photos = MongoClientPool.getInstance().collection("photos");
+        photos.deleteOne(Filters.eq("id", itemCount+1));
+    }
+
+    @Test
+    public void Test() {
+        PhotoModel model = new PhotoModel();
+        assertEquals(itemCount, model.newId());
+        Photo photo = new Photo();
+        photo.setDate(new Date());
+        photo.setLocation(new Location());
+        Photo p = model.register(photo);
+        assertEquals(itemCount+1, model.newId());
+        Photo p2 = model.findById(p.getId());
+        assertEquals(p.getId(), p2.getId());
+        assertEquals(p.getDate(), p2.getDate());
+        assertEquals(p.getDescription(), p2.getDescription());
+        assertEquals(p.getLocation().getLatitude(), p2.getLocation().getLatitude(), 0.000001);
+        assertEquals(p.getLocation().getLongitude(), p2.getLocation().getLongitude(), 0.000001);
+        assertEquals(p.getRawURI(), p2.getRawURI());
+        assertEquals(p.getRawImage(), p2.getRawImage());
+        assertEquals(p.getAuthor(), p2.getAuthor());
+    }
+}
