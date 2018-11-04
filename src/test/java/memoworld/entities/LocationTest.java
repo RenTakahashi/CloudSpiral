a0d29d729dac3edea97b220a76633b51c26d98ed
Tatsuroh Sakaguchi <tacchan.mello.ioiq@gmail.com>
2018-11-04 09:18:56 +0900

    add test for Location entity

---
+++ src/test/java/memoworld/entities/LocationTest.java
@@ -0,0 +1,23 @@
+package memoworld.entities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LocationTest {
+
+    @Test
+    public void Test() {
+        double latitude = 35.402949;
+        double longitude = 135.171579;
+        Location location1 = new Location();
+        Location location2 = new Location(latitude, longitude);
+
+        location1.setLatitude(latitude);
+        location1.setLongitude(longitude);
+        assertEquals(latitude, location1.getLatitude(), 0.000001);
+        assertEquals(longitude, location1.getLongitude(), 0.000001);
+        assertEquals(location1.getLatitude(), location2.getLatitude(), 0.000001);
+        assertEquals(location1.getLongitude(), location2.getLongitude(), 0.000001);
+    }
+}
