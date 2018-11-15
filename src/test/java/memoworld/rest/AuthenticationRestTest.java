5918daf4a9ba3a2247bde8a8821bd25a567f2462
Junichi Hayashi <j-hayasi@ist.osaka-u.ac.jp>
2018-11-15 15:44:40 +0900

    テストを JerseyTest で書き直した

--- src/test/java/memoworld/rest/AuthenticationRestTest.java
+++ src/test/java/memoworld/rest/AuthenticationRestTest.java
@@ -2,67 +2,77 @@ package memoworld.rest;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+import memoworld.entities.AccessToken;
 import memoworld.model.AccessTokenModel;
 import memoworld.model.MongoClientPool;
 import org.bson.Document;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertNotEquals;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-public class AuthenticationRestTest {
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class AuthenticationRestTest extends JerseyTest {
     private static final String TEST_REQUEST_BODY = "{\"user_id\":\"test\",\"password\":\"9f735e0df9a1ddc702bf0a1a7b83033f9f7153a00c29de82cedadc9957289b05\"}";
 
     private int accessTokenCount;
 
     @Test
-    @Ignore
     public void getAccessTokenTest() {
-        post(TEST_REQUEST_BODY)
-                .then()
-                .contentType(ContentType.JSON)
-                .statusCode(200)
-                .body("access_token", not(isEmptyOrNullString()))
-                .body("expires_at", notNullValue())
-                .body("token_type", is("Bearer"));
+        Response response = post(TEST_REQUEST_BODY);
+        assertThat(response.getStatus(), is(200));
+        AccessToken token = response.readEntity(AccessToken.class);
+        assertNotNull(token);
+        assertThat(token.getAccessToken(), not(isEmptyOrNullString()));
+        assertNotNull(token.getExpiresAt());
+        assertEquals(token.getTokenType(), "Bearer");
     }
 
     @Test
-    @Ignore
     public void getTwiceTest() {
-        String firstResponse = post(TEST_REQUEST_BODY).asString();
-        String secondResponse = post(TEST_REQUEST_BODY).asString();
+        Response firstResponse = post(TEST_REQUEST_BODY);
+        assertThat(firstResponse.getStatus(), is(200));
+        AccessToken firstResposedToken = firstResponse.readEntity(AccessToken.class);
 
-        assertNotEquals(firstResponse, secondResponse);
+        Response secondResponse = post(TEST_REQUEST_BODY);
+        assertThat(secondResponse.getStatus(), is(200));
+        AccessToken secondResposedToken = secondResponse.readEntity(AccessToken.class);
+
+        assertNotEquals(firstResposedToken.getAccessToken(), secondResposedToken.getAccessToken());
+        assertThat(secondResposedToken.getExpiresAt(), greaterThan(firstResposedToken.getExpiresAt()));
     }
 
     @Before
-    public void setUp() {
-        RestAssured.basePath = "/memoworld/api";
-
+    public void setUp() throws Exception {
+        super.setUp();
         try (AccessTokenModel model = new AccessTokenModel()) {
             accessTokenCount = model.newId();
         }
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
+        super.tearDown();
         MongoCollection<Document> tokens = MongoClientPool.getInstance().collection("access_tokens");
         tokens.deleteMany(Filters.gte("id", accessTokenCount));
     }
 
     private Response post(String body) {
-        return given()
-                .contentType(ContentType.JSON)
-                .body(body)
-                .when()
-                .post("/authentication");
+        return target("/authentication")
+                .request()
+                .post(Entity.entity(body, MediaType.APPLICATION_JSON));
+    }
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(AuthenticationRest.class);
     }
 }
