b482e37a0cc13c764cb5ce4f84851c542b8adcf2
muitimon <g1444431@gmail.com>
2018-11-17 00:06:08 +0900

    fix size of travelogue-list, header, footer.

--- src/main/webapp/js/toppage.js
+++ src/main/webapp/js/toppage.js
@@ -23,11 +23,14 @@ var initToppage = function() {
 var addTravelogue = function(title, date, author, photoID) {
 	$('#cards')
 	.append('<div class="card">'
-			+ '<img src="' + photoID + '" class="rounded float-left" class="img-thumbnail">'
+			+ '<a href="travelogue.html">'
+			+ '<img src="' + photoID + '" class="img-thumbnail">'
 			+ '<div class="card-body">'
 			+ '<h5 class="card-title">' + title + '</5>'
 			+ '<h6 class="card-subtitle">' + date + '</5>'
 			+ '<p class="card-text">by ' + author + '</p>'
+			+ '</a>'
+			+ '<img id="like-btn" class="card-thumbnail float-right" src="http://133.1.236.184:8000/jenkins/static/f9132b26/images/headshot.png">'
 			+ '</div>'
 			+ '</div>');
 }
