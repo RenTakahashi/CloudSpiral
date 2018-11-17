f30d5fb359f10eb7389ca22aa57b974e20a719e3
c2018026 R.Takahashi <c2018026@softiv.info>
2018-11-18 00:00:27 +0900

    入力したパスワードとuser_idをコンソールに表示させた

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,10 +1,30 @@
 // ログイン情報を送る
 //user_idとpasswordを送る
+//	console.log($('#user_id').val());
+//	console.log($('#password').val());
+//$('#send').click(function() {
+//	console.log($('#user_id').val());
+//	console.log($('#password').val());
+//	$.ajax({
+//		type: 'POST',
+//		url: 'http://localhost:8080/memoworld/api/authentication',
+//		data: { user_id : $('#user_id').val(), password : $('#password').val()},
+//		success: function(json) {
+//			console.log($('#user_id').val());
+//			console.log($('#password').val());
+//			console.log("clicked!!");
+//			console.log(json);
+//		}
+//	})
+//});
+
+
 $('#send').click(function() {
+	console.log($('#user_id').val());
+	console.log($('#password').val());
 	$.ajax({
-		type: 'POST',
-		url: 'http://localhost:8080/memoworld/api/authentication',
-		data: {user_id:$('#user_id').val(), password:$('#password').val()},
+		type: 'GET',
+		url: 'http://localhost:8080/memoworld/api/accounts',
 		success: function(json) {
 			console.log("clicked!!");
 			console.log(json);
