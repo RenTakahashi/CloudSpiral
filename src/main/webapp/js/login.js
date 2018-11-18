9376bc67f59215de24ebbfef87d9027a7fd9046a
c2018026 R.Takahashi <c2018026@softiv.info>
2018-11-18 23:46:03 +0900

    入力フォームの値をapiに渡して返ってきたtokenをsessionStorageに保存するところまでできた

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -5,45 +5,21 @@ $('#send').click(function() {
 	var account = {user_id: $('#user_id').val(), password: $('#password').val()};
 	console.log(account);
 	console.log(JSON.stringify(account));
+//	var access_token = '';
 	$.ajax({
 		type: 'POST',
+		contentType: 'application/json',
 		url: '/memoworld/api/authentication',
 		data: JSON.stringify(account),
 		success: function(json) {
 			console.log("success!!");
 			console.log(json);
+			console.log(json.access_token);
+			console.log(json.token_type);			
+			window.sessionStorage.setItem([$('#user_id').val()],[json.access_token]);
+			var	access_token = window.sessionStorage.getItem([$('#user_id').val()]);
+			console.log(access_token);
 		}
 	});
 });
 
-
-//$('#send').click(function() {
-//	console.log($('#user_id').val());
-//	console.log($('#password').val());
-//	let account = {user_id:$('#user_id').val(), password:$('#password').val()};	
-//	console.log(account);
-//	$.ajax({
-//		type: 'POST',
-//		url: 'http://localhost:8080/memoworld/api/authentication',
-//		data: JSON.stringify(account),
-//		success: function(json) {
-//			console.log("送信成功!!");
-//			console.log("clicked!!");
-//			console.log(json);
-//		}
-//	});
-//});
-
-
-//$('#send').click(function() {
-//	console.log($('#user_id').val());
-//	console.log($('#password').val());
-//	$.ajax({
-//		type: 'GET',
-//		url: 'http://localhost:8080/memoworld/api/accounts',
-//		success: function(json) {
-//			console.log("clicked!!");
-//			console.log(json);
-//		}
-//	})
-//});
