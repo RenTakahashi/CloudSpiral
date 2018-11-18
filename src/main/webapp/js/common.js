31a96c16c17974408578aff18d252653882ee37f
muitimon <g1444431@gmail.com>
2018-11-18 22:06:38 +0900

    fix loading template.js. write initTemplate script on template.html

---
+++ src/main/webapp/js/common.js
@@ -0,0 +1,23 @@
+var initTemplate = function(pageTitle, buttonName) {
+	//headerとfooterの準備
+	$("body").prepend(
+			'<header class="fixed-top">'
+			+ '<nav aria-label="breadcrumb">'
+			+ '<ol class="breadcrumb">'
+			+ '<li class="breadcrumb-item active" aria-current="page">Memoworld</li>'
+			+ '<li class="breadcrumb-item active" aria-current="page">'
+			+ pageTitle
+			+ '</li>'
+			+ '</ol>'
+			+ '</nav>'
+			+ '</header>');
+	if (buttonName != undefined){
+		$("body").append(
+				'<footer class="fixed-bottom">'
+				+ '<!-- ボタンの処理を変更する -->'
+				+ '<a class="btn btn-secondary float-right" href="post.html" role="button">'
+				+ buttonName
+				+ '</a>'
+				+ '</footer>');
+	}
+}
