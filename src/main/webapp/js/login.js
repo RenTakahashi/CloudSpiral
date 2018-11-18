25edde4187285c4a98657dca13ea4436adf00c69
c2018026 R.Takahashi <c2018026@softiv.info>
2018-11-18 21:44:03 +0900

    ログイン画面でpassとuser_idをapiに渡すところを模索中

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,33 +1,42 @@
 // ログイン情報を送る
 //user_idとpasswordを送る
-//	console.log($('#user_id').val());
-//	console.log($('#password').val());
-//$('#send').click(function() {
-//	console.log($('#user_id').val());
-//	console.log($('#password').val());
-//	$.ajax({
-//		type: 'POST',
-//		url: 'http://localhost:8080/memoworld/api/authentication',
-//		data: { user_id : $('#user_id').val(), password : $('#password').val()},
-//		success: function(json) {
-//			console.log($('#user_id').val());
-//			console.log($('#password').val());
-//			console.log("clicked!!");
-//			console.log(json);
-//		}
-//	})
-//});
-
 
 $('#send').click(function() {
 	console.log($('#user_id').val());
 	console.log($('#password').val());
+	console.log($('#name').val());
+	//	let data = {
+//		      user_id: $('#user_id').val(),
+//		      password: $('#password').val()
+//	};
+//	console.log(data);
 	$.ajax({
-		type: 'GET',
-		url: 'http://localhost:8080/memoworld/api/accounts',
+		type: 'POST',
+        contentType: 'application/JSON',
+        dataType : 'JSON',
+		url: 'http://localhost:8080/memoworld/api/accounts',		
+		data: { user_id : $('#user_id').val(), password : $('#password').val(), name : $('#name').val()},
 		success: function(json) {
+			console.log("送信成功!!");
+			console.log($('#user_id').val());
+			console.log($('#password').val());
+			console.log($('#name').val());
 			console.log("clicked!!");
 			console.log(json);
 		}
 	})
 });
+
+
+//$('#send').click(function() {
+//	console.log($('#user_id').val());
+//	console.log($('#password').val());
+//	$.ajax({
+//		type: 'GET',
+//		url: 'http://localhost:8080/memoworld/api/accounts',
+//		success: function(json) {
+//			console.log("clicked!!");
+//			console.log(json);
+//		}
+//	})
+//});
