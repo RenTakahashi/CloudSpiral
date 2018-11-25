04949d09819e7e7fa0ed64f81913f097754c9f95
muitimon <g1444431@gmail.com>
2018-11-25 16:04:23 +0900

    fix date-format

--- src/main/webapp/js/toppage.js
+++ src/main/webapp/js/toppage.js
@@ -5,6 +5,7 @@ var endpoint = "./api/";
 var createTravelogueList = function(travelogue) {
 	var photo = getPhoto(travelogue.photos[0]);
 	var cardWidth = $("#travelogue-lists").width() * 3/4;
+	var date = getDate(travelogue.date);
 	console.log(cardWidth);
 	$("#travelogue-lists").append(
 			'<a href="card.html?' + travelogue.id + '" style="color: gray; text-decoration:none">'
@@ -12,7 +13,7 @@ var createTravelogueList = function(travelogue) {
 			+ '<img class="center-block" style="width: 100%; height:' + cardWidth + 'px; object-fit: contain;" src="' + photo + '">'
 			+ '<div class="card-body card-img-overlay">'
 			+ '<div class="text-monospace" style="background: rgba(255,255,255,0.6);">'
-			+ '<small class="text-muted">' + travelogue.date + '</small>'
+			+ '<small class="text-muted">' + date + '</small>'
 			+ '<h5 class="card-title">' + travelogue.title.substr(0,10) + '</h5>'
 			+ '<p class="text-right" style="font-size: 0.6rem">by ' + travelogue.author + '</p>'
 			+ '</div></div>'
@@ -38,6 +39,14 @@ var createTravelogueLists = function() {
  * */
 var getPhoto = function(photos) {
 	return 'data:image/jpeg;base64,' + photos.rawImage;
+}
+
+var getDate = function(date) {
+	var formatDate = new Date(date)
+	var y = formatDate.getFullYear();
+	var m = formatDate.getMonth() + 1;
+	var d = formatDate.getDate();
+	return y + '/' + m + '/' + d; 
 }
 
 var changeFooterURL = function(){
